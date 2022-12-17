f90ec5edffb4d43c493333308533619424611317
pragmaxim <pragmaxim@gmail.com>
2022-12-17 22:11:06 +0100

    alert plugin cleanup

--- modules/alert-plugin/src/main/scala/org/ergoplatform/uexplorer/plugin/alert/AlertPlugin.scala
+++ modules/alert-plugin/src/main/scala/org/ergoplatform/uexplorer/plugin/alert/AlertPlugin.scala
@@ -12,51 +12,17 @@ import org.ergoplatform.uexplorer.plugin.Plugin.{UtxoStateWithPool, UtxoStateWit
 import org.slf4j.{Logger, LoggerFactory}
 import reactor.core.publisher.{Flux, Mono}
 import retry.Policy
-
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.collection.immutable.ListMap
 import scala.jdk.CollectionConverters.*
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.jdk.FutureConverters.*
 import scala.util.Try
 import scala.concurrent.duration.*
 
 class AlertPlugin extends Plugin {
-  protected val logger: Logger = LoggerFactory.getLogger(getClass.getName)
-
-  private val retryPolicy: Policy = retry.Backoff(3, 3.second)
-
-  private var discordToken: String                 = null
-  private var discordAlertChannelId: String        = null
-  private var client: Future[GatewayDiscordClient] = null
-
-  // message can have max 2000 characters
-  private def sendMessages(msgs: List[String]): Future[Unit] =
-    if (msgs.isEmpty)
-      Future.successful(())
-    else
-      retryPolicy.apply { () =>
-        client
-          .flatMap { client =>
-            client
-              .getChannelById(Snowflake.of(discordAlertChannelId))
-              .ofType(classOf[MessageChannel])
-              .flux()
-              .flatMap { channel =>
-                Mono
-                  .just(msgs.asJava)
-                  .flatMapMany(Flux.fromIterable)
-                  .flatMap { msg =>
-                    logger.info(msg)
-                    channel.createMessage(msg)
-                  }
-              }
-              .collectList()
-              .toFuture
-              .asScala
-          }
-          .map(_ => ())
-      }(retry.Success.always, global)
+  protected val logger: Logger         = LoggerFactory.getLogger(getClass.getName)
+  private val discord: Future[Discord] = Discord.fromEnv
 
   private lazy val detectors = List(
     new HighValueDetector(200 * 1000, 500 * 1000)
@@ -64,49 +30,32 @@ class AlertPlugin extends Plugin {
 
   def name: String = "Alert Plugin"
 
-  def init: Future[Unit] = Future
-    .fromTry(
-      Try {
-        discordToken = Option(System.getenv("DISCORD_TOKEN"))
-          .filterNot(_ == "")
-          .getOrElse(throw new IllegalStateException("DISCORD_TOKEN must be exported"))
-        discordAlertChannelId = Option(System.getenv("DISCORD_ALERT_CHANNEL"))
-          .filterNot(_ == "")
-          .getOrElse(throw new IllegalStateException("DISCORD_ALERT_CHANNEL must be exported"))
-      }
-    )
-    .flatMap { _ =>
-      client = DiscordClient
-        .builder(discordToken)
-        .build
-        .login
-        .toFuture
-        .asScala
-      client.map(_ => ())
-    }
+  def init: Future[Unit] = discord.map(_ => ())
 
-  def close: Future[Unit] =
-    client.flatMap(_.logout().toFuture.asScala.map(_ => ()))
+  def close: Future[Unit] = discord.flatMap(_.logout)
 
   def processMempoolTx(
     newTx: ApiTransaction,
     utxoStateWoPool: UtxoStateWithoutPool,
     utxoStateWithPool: UtxoStateWithPool
   ): Future[Unit] =
-    sendMessages(
-      detectors.flatMap { detector =>
-        detector.inspectNewPoolTx(newTx, utxoStateWoPool, utxoStateWithPool)
-      }
-    )
+    discord.flatMap { c =>
+      c.sendMessages(
+        detectors.flatMap { detector =>
+          detector.inspectNewPoolTx(newTx, utxoStateWoPool, utxoStateWithPool)
+        }
+      )
+    }
 
   def processNewBlock(
     newBlock: Block,
     utxoStateWoPool: UtxoStateWithoutPool
   ): Future[Unit] =
-    sendMessages(
-      detectors.flatMap { detector =>
-        detector.inspectNewBlock(newBlock, utxoStateWoPool)
-      }
-    )
-
+    discord.flatMap { c =>
+      c.sendMessages(
+        detectors.flatMap { detector =>
+          detector.inspectNewBlock(newBlock, utxoStateWoPool)
+        }
+      )
+    }
 }
---
+++ modules/alert-plugin/src/main/scala/org/ergoplatform/uexplorer/plugin/alert/Discord.scala
@@ -0,0 +1,84 @@
+package org.ergoplatform.uexplorer.plugin.alert
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import discord4j.common.util.Snowflake
+import discord4j.core.{DiscordClient, GatewayDiscordClient}
+import discord4j.core.`object`.entity.Message
+import discord4j.core.`object`.entity.channel.MessageChannel
+import org.ergoplatform.uexplorer.db.Block
+import org.ergoplatform.uexplorer.{Address, BoxId, TxId}
+import org.ergoplatform.uexplorer.node.ApiTransaction
+import org.ergoplatform.uexplorer.plugin.Plugin
+import org.ergoplatform.uexplorer.plugin.Plugin.{UtxoStateWithPool, UtxoStateWithoutPool}
+import org.slf4j.{Logger, LoggerFactory}
+import reactor.core.publisher.{Flux, Mono}
+import retry.Policy
+
+import scala.collection.immutable.ListMap
+import scala.jdk.CollectionConverters.*
+import scala.concurrent.Future
+import scala.jdk.FutureConverters.*
+import scala.util.Try
+import scala.concurrent.duration.*
+
+class Discord(discordAlertChannelId: String, client: GatewayDiscordClient) {
+  protected val logger: Logger    = LoggerFactory.getLogger(getClass.getName)
+  private val retryPolicy: Policy = retry.Backoff(3, 3.second)
+
+  def logout: Future[Unit] = client.logout().toFuture.asScala.map(_ => ())
+
+  // message can have max 2000 characters
+  def sendMessages(msgs: List[String]): Future[Unit] =
+    if (msgs.isEmpty)
+      Future.successful(())
+    else
+      retryPolicy.apply { () =>
+        client
+          .getChannelById(Snowflake.of(discordAlertChannelId))
+          .ofType(classOf[MessageChannel])
+          .flux()
+          .flatMap { channel =>
+            Mono
+              .just(msgs.asJava)
+              .flatMapMany(Flux.fromIterable)
+              .flatMap { msg =>
+                logger.info(msg)
+                channel.createMessage(msg)
+              }
+          }
+          .collectList()
+          .toFuture
+          .asScala
+          .map(_ => ())
+      }(retry.Success.always, global)
+
+}
+
+object Discord {
+
+  def fromEnv: Future[Discord] =
+    Future
+      .fromTry(
+        Try {
+          Option(System.getenv("DISCORD_TOKEN"))
+            .filterNot(_ == "")
+            .getOrElse(throw new IllegalStateException("DISCORD_TOKEN must be exported"))
+        }
+      )
+      .flatMap { discordToken =>
+        DiscordClient
+          .builder(discordToken)
+          .build
+          .login
+          .toFuture
+          .asScala
+      }
+      .map { client =>
+        val discordAlertChannelId =
+          Option(System.getenv("DISCORD_ALERT_CHANNEL"))
+            .filterNot(_ == "")
+            .getOrElse(throw new IllegalStateException("DISCORD_ALERT_CHANNEL must be exported"))
+        new Discord(discordAlertChannelId, client)
+      }
+}
