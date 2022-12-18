1ad3927205098bcc81cdc9f236f685d486c0f065
pragmaxim <pragmaxim@gmail.com>
2022-12-18 09:32:44 +0100

    cleaning up HighValueDetector

--- modules/alert-plugin/src/main/scala/org/ergoplatform/uexplorer/plugin/alert/HighValueDetector.scala
+++ modules/alert-plugin/src/main/scala/org/ergoplatform/uexplorer/plugin/alert/HighValueDetector.scala
@@ -5,7 +5,7 @@ import org.ergoplatform.uexplorer.{Address, BoxId, Const}
 import org.ergoplatform.uexplorer.node.ApiTransaction
 import org.ergoplatform.uexplorer.plugin.Plugin.{UtxoStateLike, UtxoStateWithPool, UtxoStateWithoutPool}
 import org.ergoplatform.uexplorer.plugin.alert.Detector.AlertMessage
-
+import HighValueDetector.*
 import java.text.DecimalFormat
 import scala.collection.immutable.ArraySeq
 
@@ -15,63 +15,50 @@ class HighValueDetector(txErgValueThreshold: Long, blockErgValueThreshold: Long)
     tx: ApiTransaction,
     utxoStateWoPool: UtxoStateWithoutPool,
     utxoStateWithPool: UtxoStateWithPool
-  ): List[AlertMessage] = {
-    val explorerAddress = s"https://explorer.ergoplatform.com/en/transactions/${tx.id}"
-    HighValueDetector
-      .detectHighValue(
-        tx.inputs.iterator.map(_.boxId),
-        tx.outputs.map(o => o.address -> o.value),
-        utxoStateWithPool,
-        txErgValueThreshold
-      )
-      .map(msg => s"$explorerAddress $msg")
-      .toList
-  }
-
-  def inspectNewBlock(
-    newBlock: Block,
-    utxoStateWoPool: UtxoStateWithoutPool
-  ): List[AlertMessage] = {
-    val explorerAddress = s"https://explorer.ergoplatform.com/en/blocks/${newBlock.header.id}"
-    HighValueDetector
-      .detectHighValue(
-        newBlock.inputs.iterator.map(_.boxId),
-        newBlock.outputs.collect { case o if o.address != Const.genesisEmissionAddress => o.address -> o.value },
-        utxoStateWoPool,
-        blockErgValueThreshold
-      )
-      .map(msg => s"$explorerAddress $msg")
-      .toList
-  }
-}
-
-object HighValueDetector {
-
-  private val valueFormat = new DecimalFormat("#,###")
-  private val nanoOrder   = 1000000000d
-
-  def detectHighValue(
-    inputBoxIds: Iterator[BoxId],
-    utxos: ArraySeq[(Address, Long)],
-    utxoState: UtxoStateLike,
-    ergValueThreshold: Long
-  ): Option[AlertMessage] =
-    Option(utxos.iterator.map(_._2).sum)
-      .filter(_ >= ergValueThreshold * nanoOrder)
+  ): List[AlertMessage] =
+    Option(tx.outputs.iterator.map(_.value).sum)
+      .filter(_ >= txErgValueThreshold * nanoOrder)
       .map { value =>
-        val inputAddresses = inputBoxIds.flatMap(utxoState.addressByUtxo.get).toSet
+        val inputAddresses = tx.inputs.iterator.map(_.boxId).flatMap(utxoStateWithPool.addressByUtxo.get).toSet
         val inputAddressesSum =
-          inputAddresses.flatMap(utxoState.utxosByAddress.get).foldLeft(0L) { case (acc, valueByBox) =>
+          inputAddresses.flatMap(utxoStateWithPool.utxosByAddress.get).foldLeft(0L) { case (acc, valueByBox) =>
             acc + valueByBox.values.sum
           }
-        val outputAddresses = utxos.iterator.map(_._1).toSet
+        val outputAddresses = tx.outputs.iterator.map(_.address).toSet
         val outputAddressesSum =
-          outputAddresses.flatMap(utxoState.utxosByAddress.get).foldLeft(0L) { case (acc, valueByBox) =>
+          outputAddresses.flatMap(utxoStateWithPool.utxosByAddress.get).foldLeft(0L) { case (acc, valueByBox) =>
             acc + valueByBox.values.sum
           }
         val fmtValue              = valueFormat.format(value / nanoOrder)
         val fmtInputAddressesSum  = valueFormat.format(inputAddressesSum / nanoOrder)
         val fmtOutputAddressesSum = valueFormat.format(outputAddressesSum / nanoOrder)
-        s"${inputAddresses.size} addresses with total of $fmtInputAddressesSum ===> $fmtValue ===> ${outputAddresses.size} addresses with total of $fmtOutputAddressesSum"
+        s"${inputAddresses.size} addresses with total of $fmtInputAddressesSum Erg ===> $fmtValue Erg ===> ${outputAddresses.size} addresses with total of $fmtOutputAddressesSum Erg"
       }
+      .map(msg => s"https://explorer.ergoplatform.com/en/transactions/${tx.id} $msg")
+      .toList
+
+  def inspectNewBlock(
+    newBlock: Block,
+    utxoStateWoPool: UtxoStateWithoutPool
+  ): List[AlertMessage] =
+    Option(newBlock.outputs.iterator.map(_.value).sum)
+      .filter(_ >= blockErgValueThreshold * nanoOrder)
+      .map { value =>
+        val outputAddresses = newBlock.outputs.iterator.map(_.address).toSet
+        val outputAddressesSum =
+          outputAddresses.flatMap(utxoStateWoPool.utxosByAddress.get).foldLeft(0L) { case (acc, valueByBox) =>
+            acc + valueByBox.values.sum
+          }
+        val fmtValue              = valueFormat.format(value / nanoOrder)
+        val fmtOutputAddressesSum = valueFormat.format(outputAddressesSum / nanoOrder)
+        s"$fmtValue Erg ===> ${outputAddresses.size} addresses with total of $fmtOutputAddressesSum Erg"
+      }
+      .map(msg => s"https://explorer.ergoplatform.com/en/blocks/${newBlock.header.id} $msg")
+      .toList
+}
+
+object HighValueDetector {
+
+  val valueFormat = new DecimalFormat("#,###")
+  val nanoOrder   = 1000000000d
 }
