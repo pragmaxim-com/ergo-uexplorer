4e710df37fa059294f36fed4595a0270f983f8e9
pragmaxim <pragmaxim@gmail.com>
2022-12-19 19:13:49 +0100

    revisiting cassandra deplyment

--- docker/start-indexing.sh
+++ docker/start-indexing.sh
@@ -15,18 +15,22 @@ while true; do
               exit 1
           elif [ "$V_CPU_COUNT" -ge 16 ] && [ "$MEM_TOTAL" -gt 30000000 ]
           then
+            echo "This is not tested yet and it probably requires changing GC from CMS to G1"
+            export CASSANDRA_HEAP_NEWSIZE=2G
             export CASSANDRA_MAX_HEAP_SIZE=16G
             export ERGO_MAX_HEAP=4G
             export BACKEND_INDEXING_PARALLELISM=2
           elif [ "$MEM_TOTAL" -gt 16000000 ]
           then
             echo "Please close all memory intensive processes like Browser, IDE, etc. (OOM killer might kick in) until syncing finishes"
+            export CASSANDRA_HEAP_NEWSIZE=1G
             export CASSANDRA_MAX_HEAP_SIZE=9G
             export ERGO_MAX_HEAP=2G
             export BACKEND_INDEXING_PARALLELISM=1
           fi
           break;;
         [Nn]* )
+          export CASSANDRA_HEAP_NEWSIZE=512M
           export CASSANDRA_MAX_HEAP_SIZE=1G
           export ERGO_MAX_HEAP=1G
           break;;
